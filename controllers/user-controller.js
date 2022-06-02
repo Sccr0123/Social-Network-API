ff60c20010c8d1fbbe8bf23c25a1f6d26b6eafde
Zachary Thomas <sccr0123@gmail.com>
2022-06-02 19:19:55 -0400

    Created User Controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,63 @@
+const { User } = require("../models");
+
+const UserController = {
+	getAllUser(req, res) {
+		User.find({})
+			.populate({
+				path: "thoughts",
+				select: "-__v",
+			})
+			.select("-__v")
+			.sort({ _id: -1 })
+			.then((dbUserData) => res.json(dbUserData))
+			.catch((err) => {
+				console.log(err);
+				res.sendStatus(400);
+			});
+	},
+
+	getUserById({ params }, res) {
+		User.findOne({ _id: params.id })
+			.populate({
+				path: "thoughts",
+				select: "-__v",
+			})
+			.select("-__v")
+			.then((dbUserData) => res.json(dbUserData))
+			.catch((err) => {
+				console.log(err);
+				res.sendStatus(400);
+			});
+	},
+
+	createUser({ body }, res) {
+		User.create(body)
+			.then((dbUserData) => res.json(dbUserData))
+			.catch((err) => res.json(err));
+	},
+
+	updateUser({ params, body }, res) {
+		User.findOneAndUpdate({ _id: params.id }, body, {
+			new: true,
+			runValidators: true,
+		})
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({
+						message: "No User found with this id!",
+					});
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch((err) => res.status(400).json(err));
+	},
+
+	deleteUser({ params }, res) {
+		Pizza.findOneAndDelete({ _id: params.id })
+			.then((dbUserData) => res.json(dbUserData))
+			.catch((err) => res.json(err));
+	},
+};
+
+module.exports = UserController;
