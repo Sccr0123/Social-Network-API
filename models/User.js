7dd3f2f32f801a0ce44cc5a11b4f99dd5ca695e7
Zachary Thomas <sccr0123@gmail.com>
2022-06-02 20:19:09 -0400

    Added Email Validation

--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const UserSchema = new Schema(
 			type: String,
 			unique: true,
 			required: true,
-			// validate: [isEmail, "Invalid Email"],
+			match: [/.+@.+\..+/],
 		},
 		thoughts: [
 			{
