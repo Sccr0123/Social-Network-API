8d02b78ee751a0f734a8b2d7bc81793844279a2f
Zachary Thomas <sccr0123@gmail.com>
2022-06-02 20:00:26 -0400

    Created Thought Routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const {
+	addThought,
+	addReaction,
+	removeThought,
+	removeReaction,
+} = require("../../controllers/thought-controller");
+
+router.route("/:userId").post(addThought);
+
+router.route("/userId/:toughtId").put(addReaction).delete(removeThought);
+
+router.route("/userId/:thoughtId/:reactionId").delete(removeReaction);
+
+module.exports = router;
